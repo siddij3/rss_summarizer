e10d679c6900e290bc5db1d3f9860e85b5601bf6
Junaid Siddiqui <junaidas97@gmail.com>
2023-09-08 15:54:09 -0400

    first commit

---
+++ scrape_articles.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import json
+from html.parser import HTMLParser
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import re
+
+url = f"https://api.start.me/widgets/64657916,64619065,64814145/articles" 
+url2 = f"https://api.start.me/widgets/63871721/articles"
+
+with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) as driver:
+    driver.get(url)
+    first = driver.page_source
+
+    driver.get(url2)
+    second = driver.page_source
+
+rss_1 = json.loads(BeautifulSoup(first).get_text())
+rss_2 = json.loads(BeautifulSoup(second).get_text())
+
